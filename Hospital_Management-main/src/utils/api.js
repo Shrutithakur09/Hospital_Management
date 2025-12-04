e99cf2e45db93d6bcdf18db5e8346f40121d670a
Shrutithakur30 <sshruti_be23@thapar.edu>
2025-12-05 02:04:42 +0530

    Fix axios api baseURL string

--- Hospital_Management-main/src/utils/api.js
+++ Hospital_Management-main/src/utils/api.js
@@ -2,6 +2,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://hospital-management-16wx.onrender.com
-", // backend ka URL
+  baseURL: "https://hospital-management-16wx.onrender.com", // backend ka URL
 });
